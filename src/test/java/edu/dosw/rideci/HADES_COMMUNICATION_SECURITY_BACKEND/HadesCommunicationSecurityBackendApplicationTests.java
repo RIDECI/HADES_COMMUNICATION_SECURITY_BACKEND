85749c4dcd622a67b95cb9fa0919f0672aac5bdd
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-11-10 22:45:18 -0500

    22-feat: agregando CI/CD al backend

--- src/test/java/edu/dosw/rideci/HADES_COMMUNICATION_SECURITY_BACKEND/HadesCommunicationSecurityBackendApplicationTests.java
+++ src/test/java/edu/dosw/rideci/HADES_COMMUNICATION_SECURITY_BACKEND/HadesCommunicationSecurityBackendApplicationTests.java
@@ -3,7 +3,9 @@ package edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class HadesCommunicationSecurityBackendApplicationTests {
 
 	@Test
