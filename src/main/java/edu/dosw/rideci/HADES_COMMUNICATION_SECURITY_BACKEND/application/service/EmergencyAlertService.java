3334107a8a0346ef5365556ccd1b5848f8c2dea9
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2025-11-24 18:54:49 -0500

    feat-HAD-37:implementacion con Rabbit

--- src/main/java/edu/dosw/rideci/HADES_COMMUNICATION_SECURITY_BACKEND/application/service/EmergencyAlertService.java
+++ src/main/java/edu/dosw/rideci/HADES_COMMUNICATION_SECURITY_BACKEND/application/service/EmergencyAlertService.java
@@ -2,9 +2,11 @@ package edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.service
 
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.dtos.request.EmergencyAlertRequest;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.dtos.response.EmergencyAlertResponse;
+import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.events.EmergencyAlertEvent;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.mappers.EmergencyAlertMapper;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.ports.in.ActivateEmergencyButtonUseCase;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.ports.out.EmergencyAlertRepositoryPort;
+import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.application.ports.out.EventPublisher;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.domain.entities.EmergencyAlert;
 import edu.dosw.rideci.HADES_COMMUNICATION_SECURITY_BACKEND.domain.valueobjects.LocationVO;
 import lombok.AllArgsConstructor;
@@ -20,7 +22,7 @@ public class EmergencyAlertService implements ActivateEmergencyButtonUseCase {
 
     private final EmergencyAlertRepositoryPort emergencyAlertRepository;
     private final EmergencyAlertMapper mapper;
-
+    private final EventPublisher eventPublisher;
 
 
     @Override
@@ -35,7 +37,20 @@ public class EmergencyAlertService implements ActivateEmergencyButtonUseCase {
         alert.setCreatedAt(LocalDateTime.now());
         alert.setAdditionalInfo("Emergency button activated");
 
+
         emergencyAlertRepository.save(alert);
+
+
+        EmergencyAlertEvent event = EmergencyAlertEvent.builder()
+                .id(alert.getId())
+                .tripId(alert.getTripId())
+                .userId(alert.getUserId())
+                .type("EMERGENCY")
+                .createdAt(alert.getCreatedAt())
+                .build();
+
+
+        eventPublisher.publish(event, "emergency.created");
     }
 
     public EmergencyAlertResponse getAlert(String id) {
