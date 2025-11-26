476ac4b289ac7be8724a3a7705a122c37cdd03cb
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-11-25 21:31:23 -0500

    fix - conexion rabbit

--- src/main/java/edu/dosw/rideci/HadesCommunicationSecurityBackendApplication.java
+++ src/main/java/edu/dosw/rideci/HadesCommunicationSecurityBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableRabbit
+
 public class HadesCommunicationSecurityBackendApplication {
 
 	public static void main(String[] args) {
