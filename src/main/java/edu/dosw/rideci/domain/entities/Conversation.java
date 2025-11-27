ba3e42aa8b83168c10ab5dc3c4bf8f2a431a9d5d
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-11-27 01:05:57 -0500

    22-fix evento de crear viaje corregido

--- src/main/java/edu/dosw/rideci/domain/entities/Conversation.java
+++ src/main/java/edu/dosw/rideci/domain/entities/Conversation.java
@@ -20,6 +20,7 @@ public class Conversation {
     private String id = UUID.randomUUID().toString();
     private Long tripId;
     private Long organizerId;
+    private Long driveId;
     private TravelType type;
     private Status travelStatus;
     private boolean active = false;
